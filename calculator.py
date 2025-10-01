202b572525273d5864145d1096975e1d20fe6c75
Riya Singh <riyasinghparmar009@gmail.com>
2025-10-02 02:38:40 +0530

    Added square and square root functions to calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 def add(x,y):
     return x+y
 def subtract(x,y):
@@ -9,26 +10,40 @@ def divide(x,y):
         print("Error! Division by 0 not defined") 
     else:
         return x/y
-    return x/y
+def square(x):
+    return x*x
+def sqr_root(x):
+    if x<0:
+        print("Error! Square root of negative number not defined")
+    else:
+        return math.sqrt(x)
 
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+print("5. Square")
+print("6. Square Root")
 
-choice = input("Enter operation to be performed (1/2/3/4): ")
+choice = input("Enter operation to be performed (1/2/3/4/5/6): ")
+if choice in ['1','2','3','4']:
+    x = float(input("Enter first number: "))
+    y = float(input("Enter second number: "))
 
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
-
-if choice == '1':
-    print("Result:", add(x, y))
-elif choice == '2':
-    print("Result:", subtract(x, y))
-elif choice == '3':
-    print("Result:", multiply(x, y))
-elif choice == '4':
-    print("Result:", divide(x, y))
+   if choice == '1':
+        print("Result:", add(x, y))
+   elif choice == '2':
+       print("Result:", subtract(x, y))
+   elif choice == '3':
+        print("Result:", multiply(x, y))
+   elif choice == '4':
+       print("Result:", divide(x, y))
+   elif choice == '5':
+       x = float(input("Enter a number: "))
+       print("Result:", square(x))
+   elif choice == '6':
+         x = float(input("Enter a number: "))
+         print("Result:", sqr_root(x))
 else:
     print("Invalid input")
